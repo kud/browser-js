922524021e2563ea1fad5f8dca9c08cda86ef84e
Erwann Mest <m@kud.io>
2015-01-30 13:43:03 +0100

    Update wrapper.

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-module.exports = require('./browser.js')
+;(function (root, factory) {
+  if (typeof exports === 'object') {
+    module.exports = factory(require('browser-js'))
+  } else {
+    // root.returnExportsGlobal = factory(root.Browser) // sorry kids, I don't know how it really works so I added 'root.Browser'
+    root.Browser = root.returnExportsGlobal = factory(root.Browser)
+  }
+}(this, function () {
+
+  return {
+
+    visit: function( url, top ) {
+      var top = top || false
+
+      if ( top ) window.top.location.href = url
+      else window.location.href = url
+    },
+
+    newTab: function( url ) {
+      window.open(url,'_blank')
+    },
+
+    reload: function( forced ) {
+      var forced = forced || false
+      document.location.reload( forced )
+    },
+
+    redirectToOriginal: function() {
+      window.top.location.href = window.location.href
+    },
+
+    preventEmbedded: function() {
+      if ( this.isEmbedded() ) {
+        this.redirectToOriginal()
+      }
+    },
+
+    isEmbedded: function() {
+      return window.top !== window.self
+    },
+
+    isPoppedUp: function() {
+      return !(window.opener === null || window.opener === undefined)
+    }
+
+  }
+
+}))
